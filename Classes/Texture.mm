f10b39714ed923c11cdb44cf56a0a1cbd236a39c
wkoswara <willy.koswara@gmail.com>
2012-03-28 00:46:31 -0700

    Implemented alpha blending/transparency

--- Classes/Texture.mm
+++ Classes/Texture.mm
@@ -48,6 +48,8 @@
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, spriteData);
 	
     free(spriteData);        
