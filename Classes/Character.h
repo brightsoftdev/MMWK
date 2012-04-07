12d18464ea8742a995f6c35639cca14f838c7c6a
Morkos <morkos.cairo@gmail.com>
2012-04-06 23:13:30 -0700

    Adding basic narrow collision detection mechanism. Abstracted Player class. Add Prop class and collidable interface

---
+++ Classes/Character.h
@@ -0,0 +1,35 @@
+//
+//  Character.h
+//  DragonEye
+//
+//  Created by Mark Mikhail on 4/6/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Prop.h"
+#import "PropState.h"
+#import "PhysicsEngine.h"
+#import "SpriteSheet.h"
+
+@interface Character : Prop {
+	
+	PhysicsEngine * physicsEngine;
+	
+	PlayerState currentState;
+	Direction currentDirection;
+	SpriteSheet *sprite;
+	CADisplayLink *displayLink;
+	
+	// Current matrix index in the sprite sheet
+	uint spsheetRowInd, spsheetColInd;
+}
+
+@property (nonatomic, assign) PlayerState currentState;
+@property (nonatomic, retain) SpriteSheet *sprite;
+@property (nonatomic, assign) uint spsheetRowInd, spsheetColInd;
+@property (nonatomic, assign) CADisplayLink *displayLink;
+@property (nonatomic, retain) PhysicsEngine *physicsEngine;
+@property (nonatomic, assign) Direction currentDirection;
+
+
+@end
